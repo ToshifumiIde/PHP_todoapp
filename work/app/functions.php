bc3540e205908ff5860ba33165707495728d7270
ToshifumiIde <toshifumiide198765@gmail.com>
2021-04-16 21:19:34 +0900

    create Database class

--- work/app/functions.php
+++ work/app/functions.php
@@ -39,24 +39,3 @@ function addTodo($pdo)
   $stmt->bindValue("title" , $title , PDO::PARAM_STR);
   $stmt->execute();
 }
-
-function getPdoInstance(){
-
-  try{
-    $pdo = new PDO(
-      DSN, 
-      DB_USER,
-      DB_PASS,
-      [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_OBJ,
-        PDO::ATTR_EMULATE_PREPARES=>false,
-      ]
-    );
-    return $pdo;
-  }catch(PDOException $e){
-    echo $e->getMessage();
-    exit;
-  };
-
-}
