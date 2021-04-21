e61b8cb80e312ad55c46dd6dca8ca877ab7725fd
ToshifumiIde <toshifumiide198765@gmail.com>
2021-04-21 16:20:42 +0900

    create name space

--- work/public/index.php
+++ work/public/index.php
@@ -3,6 +3,12 @@ require_once(__DIR__ . "/../app/config.php");
 //現在のファイルが存在するディレクトリの絶対パスを示す特殊なキーワードを使う。
 //__DIR__は最後にスラッシュがつかないから、スラッシュの付け忘れに注意
 
+use MyApp\Database;
+//Databaseクラスが出てきたら、MyApp\をつけてくれる
+use MyApp\Todo;
+use MyApp\Utils;
+//なお、TokenクラスではTokenクラスのメソッドを使っているTodo.phpの先頭で、既にMyAppの名前空間が定義されているため、こちらのTokenクラスにはMyAppをつける必要はない。
+
 $pdo = Database::getInstance();
 
 $todo = new Todo($pdo);
