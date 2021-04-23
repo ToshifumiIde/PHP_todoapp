2fe39c1c3ab36057731b0c1d062b49fd251005c1
ToshifumiIde <toshifumiide198765@gmail.com>
2021-04-23 11:33:34 +0900

    modify checkbox ui in css

--- work/public/index.php
+++ work/public/index.php
@@ -50,15 +50,12 @@ $todos = $todo->getAll();
             data-token="<?=Utils::h($_SESSION['token']); ?>"
             <?=$todo->is_done ? "checked" : "";?> 
           >
-          <span class="<?=$todo->is_done ? "done" : "" ;?>">
-            <?=Utils::h($todo->title); ?>
-          </span>
-        <form action="?action=delete" method="post" class="delete-form">
-          <span class="delete">x</span>
-          <input type="hidden" name="id" value="<?= Utils::h($todo->id); ?>">
-          <input type="hidden" name="token" value="<?= Utils::h($_SESSION['token']); ?>">
-        </form>
-        
+          <span><?=Utils::h($todo->title); ?></span>
+          <form action="?action=delete" method="post" class="delete-form">
+            <span class="delete">x</span>
+            <input type="hidden" name="id" value="<?= Utils::h($todo->id); ?>">
+            <input type="hidden" name="token" value="<?= Utils::h($_SESSION['token']); ?>">
+          </form>
         </li>
       <?php endforeach; ?>
     </ul>
